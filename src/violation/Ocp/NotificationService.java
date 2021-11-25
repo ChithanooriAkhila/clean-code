59d50b2144b38e30e9ee973b623f973011062547
chithanoori akhila <akhila.chithanoori@zemosolabs.com>
2021-11-25 16:53:16 +0530

    ocp violation

---
+++ src/violation/Ocp/NotificationService.java
@@ -0,0 +1,12 @@
+package violation.Ocp;
+
+public class NotificationService {
+    public void sendOTP(String medium){
+        if(medium.equals("email")){
+            //write logic
+        }
+        if(medium.equals("mobile")){
+            //write logic
+        }
+    }
+}
